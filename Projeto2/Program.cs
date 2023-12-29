42b6c259889e523e0f8537eed964161375f39452
AndersonSheep <61760830+AndersonSheep@users.noreply.github.com>
2023-12-29 10:53:41 -0300

    Projeto3 operadores lógicos e condicionais

--- Projeto2/Program.cs
+++ Projeto2/Program.cs
@@ -1,12 +1,32 @@
-﻿using Projeto2.Models;
+﻿// Importanto uma classe
+using Projeto2.Models;
+// Iniciando uma classe
 Pessoa pessoa1 = new()
 {
     Nome = "Dinho",
     Idade = 29
 };
-
 pessoa1.Apresentar();
 
+// Metodo DateTime
 DateTime dataAtual = DateTime.Now;
 Console.WriteLine(dataAtual);
 Console.WriteLine(dataAtual.ToString("dd/MM/yyy"));
+
+// Metodo de conversão de valores Convert e Parse
+int a = Convert.ToInt32("5");
+int b = int.Parse("5");
+
+// Metodo para converter pra string ToString()
+string c = a.ToString();
+
+// Metodo para pegar o valor máximo de um tipo
+int d = int.MaxValue;
+
+// TryParse, metodo para verificar antes de converter
+string e = "9-";
+int.TryParse(e, out a);
+
+// Espera o usuário digitar um comando Console.ReadLine()
+System.Console.WriteLine("Digite uma letra: ");
+//string letra = Console.ReadLine();
---
+++ Projeto3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Operadores lógicos e condicionais > < = == >= <=
+int quantidadeEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantiadde em estoque: {quantidadeEstoque}");
+Console.WriteLine($"Quantiadde compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realziar a venda? {possivelVenda}");
+
+if (possivelVenda){
+    Console.WriteLine("Venda realizada.");
+}
+else if (quantidadeCompra == 0){
+    System.Console.WriteLine("Venda inválida");
+}
+else{
+    Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque");
+}
+
+// If vs Switch Case, verificar se é uma vogal ou não
+string letra = "a";
+
+if (letra == "a" ||
+    letra == "e" ||
+    letra == "i" ||
+    letra == "o" ||
+    letra == "u"){
+        System.Console.WriteLine("É uma vogal");
+}else{
+    System.Console.WriteLine("Não é vogal");
+}
+
+switch (letra){
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        System.Console.WriteLine("Vogal");
+    break;
+    
+    default:
+        System.Console.WriteLine("Não é vogal");
+    break;    
+}
